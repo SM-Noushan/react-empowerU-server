1104fe304b16a96d84fa7c8fec39912440824126
SMNoushan <noushan.md@gmail.com>
2024-06-14 00:14:11 +0600

    added database connection

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
@@ -17,6 +18,32 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.b6wqjn1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    console.log("Successfully connected to MongoDB!");
+
+    // Connect to the "empoweru" database
+    const empowerU = client.db("empowerU");
+    const scholarshipCollection = empowerU.collection("scholarships");
+  } finally {
+    //   catch (e) {
+    //     console.log(e);
+    //   }
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("EmpowerU Server Running");
