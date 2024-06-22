914653e68d7e3f0444ab9162e64c65e6f1306553
SMNoushan <noushan.md@gmail.com>
2024-06-23 00:23:30 +0600

    added hosting url to cors

--- index.js
+++ index.js
@@ -12,19 +12,15 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://ph-assignment-12-empoweru-spa.surge.sh",
+    ],
     credentials: true,
   })
 );
 app.use(express.json());
 app.use(cookieParser());
-
-// cookie
-const cookieOption = {
-  httpOnly: true,
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-  secure: process.env.NODE_ENV === "production" ? true : false,
-};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.b6wqjn1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -880,11 +876,11 @@ async function run() {
       ];
       res.send(data);
     });
-  } finally {
-    //   catch (e) {
-    //     console.log(e);
-    //   }
+  } catch (e) {
+    // finally {
+    console.log(e);
   }
+  // }
 }
 run().catch(console.dir);
 
